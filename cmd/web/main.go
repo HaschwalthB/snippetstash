63852b735de5ea74f8886b7b002f7cab483d6d47
Haschwalth <ber4gung@gmail.com>
2023-10-13 20:43:22 +0800

    add features

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
+  // this is a router
+
+  files := http.FileServer(http.Dir("./ui/static/"))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", view)
 	mux.HandleFunc("/snippet/create", create)
+  mux.Handle("/static/",http.StripPrefix("/static", files))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
