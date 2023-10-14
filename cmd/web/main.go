7e825eb92a2fc00761b02e21dc324f1a8911073c
Haschwalth <ber4gung@gmail.com>
2023-10-14 09:34:25 +0800

    make indenting

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-  "log"
+	"log"
 	"net/http"
 )
 
 func main() {
-  // this is a router
-
-  files := http.FileServer(http.Dir("./ui/static/"))
+	files := http.FileServer(http.Dir("./ui/static/"))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", view)
 	mux.HandleFunc("/snippet/create", create)
-  mux.Handle("/static/",http.StripPrefix("/static", files))
+	mux.Handle("/static/", http.StripPrefix("/static", files))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
