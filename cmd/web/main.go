64a59c5a4df281daf09040842308a4000bc980f7
Haschwalth <pekztor4gung@gmail.com>
2023-10-21 20:36:40 +0800

    fix typo

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,7 +20,7 @@ type application struct {
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	dsn := flag.String("dsn", "web:komeng@/snippetstash?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "web:komeng@/snippetstash?parseTime=true", "MySQL")
 
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO \t", log.Ldate|log.Ltime)
