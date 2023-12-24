0346d3a992b34a667c878ed83b3f1ce986e1f02a
Haschwalth <pekztor4gung@gmail.com>
2023-12-24 14:44:56 +0800

    add new custom errors

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -1,5 +1,13 @@
 package models
 
-import "errors"
+import (
+	"errors"
+)
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// ErrInvalidCredentials is used when a user tries to login with an incorrect password
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// ErrDuplicateEmail is used when a user tries to signup with an email that is already in use
+	ErrDuplicateEmail = errors.New("models : duplicate email")
+)
